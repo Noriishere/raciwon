8b634d9d8e4414ea11310be4812dfdea9a13ac48
Noriishere <noriyamashin@gmail.com>
2026-06-08 21:55:11 +0700

    Feat: Ini dia

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::prefix('admin')->middleware(['auth:web', 'verified'])->group(function () 
     Route::view('/inventory', 'inventory.index')->name('admin.inventory');
     Route::view('/stock-movements', 'stock-movements.index')->name('admin.stock-movements');
     Route::view('/expenses', 'expenses.index')->name('admin.expenses');
+    Route::view('/reports', 'reports.index')->name('admin.reports');
     // Rute admin lainnya tinggal dimasukkan di bawah sini
 });
 
