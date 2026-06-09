97b1cc263b3a9f023eceebb05d3ee227870c8727
Noriishere <noriyamashin@gmail.com>
2026-06-09 12:41:43 +0700

    Feat: Ini dia

--- resources/views/components/categories/edit-modal.blade.php
+++ resources/views/components/categories/edit-modal.blade.php
@@ -45,7 +45,7 @@
                 </div>
 
                 {{-- Form --}}
-                <form :action="'admin/categories/' + selectedCategory.id" method="POST">
+                <form :action="'{{ url('admin/categories') }}/' + selectedCategory.id" method="POST">
 
                     @csrf
                     @method('PUT')
